a35c63679ef347d890abc0134b4c8dd16e142057
leandromaroni <leandrodmaroni@gmail.com>
2025-11-05 14:24:19 -0600

    Update interview_exercise.py

--- interview_exercise.py
+++ interview_exercise.py
@@ -1,29 +1,45 @@
 import sqlite3
 import pandas as pd
-###1. Determine the number of distinct destinations connected to the airport.
-###2. Calculate the rounded average of distinct destinations for each day.
-###3. Calculate the average number of destinations per day of the week.
-###4. Identify the month with the highest number of flights.
 
-
-# Cargar CSV en pandas
+# Loads a CSV in Pandas
 df = pd.read_csv('nyc_flights_fixed.csv')
 
-# Crear una base de datos SQLite en memoria
+# Creates a SQLite memory DB
 conn = sqlite3.connect(':memory:')
 
-# Subir el dataframe como tabla SQL
+# Upload the dataframe as a SQL table
 df.to_sql('nyc_flights_fixed', conn, index=False, if_exists='replace')
 
-# Ejecutar consultas SQL
+# Execute SQL query: After each comment, the query for the item will be written.
+# Only the query that should be seen has to be called 'query'
+
+###1. Determine the number of distinct destinations connected to the airport.
 query = """
+SELECT COUNT(DISTINCT dest) distinct_destinations
+FROM nyc_flights_fixed
+WHERE origin = 'EWR'
+"""
+
+###2. Calculate the rounded average of distinct destinations for each day.
+query_2 = """
+SELECT date, ROUND(AVG(destinations),0) AS rounded_destinations
+FROM
+  (SELECT date(time_hour) as date, count(distinct dest) destinations
+  FROM nyc_flights_fixed
+  WHERE origin = 'EWR'
+  GROUP BY 1)
+"""
+
+###4. Identify the month with the highest number of flights.
+query_3 = """
 SELECT strftime('%m',date(time_hour)) as date_month, count(*)
 FROM nyc_flights_fixed
 WHERE origin = 'EWR'
 GROUP BY 1
 ORDER BY 2 DESC
-
+LIMIT 1
 """
+
 resultado = pd.read_sql_query(query, conn)
 print(resultado)
 #SELECT , COUNT(DISTINCT dest) AS destinations_count
